a89fbe3402def0f40c7a2a295574cd616c55a2c8
coder1sadhu <hansalsadhu10@gmail.com>
2026-01-02 08:13:04 +0000

    Datatypes

---
+++ datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 3) // we are nodejs , not browser
+
+console.log(3 + 3);
+
+console.log("Hansal");
+
+let name = "Hansal"; // string datatype
+let age = 21; // number datatype
+let isLoggedIn = false; // boolean datatype
+let state; // undefined datatype
+
+// number => 2 to power of 53
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable not assigned any value
+// symbol => unique identifier
+
+console.log(typeof "Hansal");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
