edfb177a6e6a7f1b2cbb19aa6ae07e4933a5b434
coder1sadhu <hansalsadhu10@gmail.com>
2026-01-09 05:54:19 +0000

    Conversion of datatypes

---
+++ conversion_operation.js
@@ -0,0 +1,28 @@
+let score = null;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false =>0;
+
+let isloggedIn = "Hansal"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false; 
+// "Hansal" => true;
+
+let somenumber = 33
+
+let stringNumber = String(somenumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
